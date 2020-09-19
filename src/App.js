f7cc49e53cc921b16abb413db8f622323a98a886
chankruze <chankruze@gmail.com>
2020-09-19 12:46:37 +0530

    [1/13] Icons
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.css'
+import { IconContext } from 'react-icons'
+import IconsContainer from './components/IconsContainer'
+
+const ICON_CONTEXT = {
+  color: 'blue',
+  size: '5rem'
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <IconContext.Provider value={ICON_CONTEXT}>
+      <div className="App">
+        {/* Icons */}
+        <IconsContainer />
+      </div>
+    </IconContext.Provider>
   );
 }
 
---
+++ src/components/IconsContainer.js
@@ -0,0 +1,21 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sat Sep 19 2020 12:30:04 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react'
+import { FaReact } from "react-icons/fa"
+import { MdCardGiftcard } from "react-icons/md"
+
+function IconsContainer() {
+    return (
+        <div>
+            <FaReact size="10rem" color="lightblue" />
+            <MdCardGiftcard />
+        </div>
+    )
+}
+
+export default IconsContainer
